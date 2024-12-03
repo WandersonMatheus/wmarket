b4ba5b162c298f6982f40ab3d2f1dad451aebc95
Wanderson Matheus <126834974+wmatheus190900@users.noreply.github.com>
2024-12-03 12:40:31 -0300

    features:finalized body e style of loginpage,start creating forms

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { DefaultLoginLayoutComponent } from "../../components/default-login-layout/default-login-layout.component";
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [DefaultLoginLayoutComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
